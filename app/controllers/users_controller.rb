2095da3d2cee436f5c408e0b3484fa3689d95838
Hai Nguyen <hmn91733@gmail.com>
2014-02-01 15:24:11 -0800

    Add css to show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,30 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to action: 'index'
   end
 
   def create
     @user = User.new(users_params)
     if @user.save
-      redirect_to action: 'new'
+      redirect_to action: 'index'
       flash[:notice] = "Account created!"
     else
       render 'new'
@@ -14,6 +32,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   # Strong params
   def users_params
