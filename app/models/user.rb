ebd6115b83ef516738e24e4518022afa350a0b1b
Hai Nguyen <hmn91733@gmail.com>
2014-02-07 18:18:36 -0800

    Add stripes

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false}
-  # validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
 
   def authenticated?(pwd)
     self.hashed_password == BCrypt::Engine.hash_secret(pwd, self.salt)
